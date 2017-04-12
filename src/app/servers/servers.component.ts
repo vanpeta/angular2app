3c2caabf177057fe14f70b631c8f74fd06b7c730
vanpeta <carlosallendesanz@gmail.com>
2017-04-12 16:22:48 -0700

    finsihed section 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: '[app-servers]',
-  selector: '.app-servers',
+  // selector: '[app-servers]', /*selector syntax for a html atribute */
+  selector: '.app-servers', /*selector syntax for a html class */
   template: `
     <app-server></app-server>
     <app-server></app-server>
